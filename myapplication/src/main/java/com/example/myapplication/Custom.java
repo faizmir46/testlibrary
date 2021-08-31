af404f7539eb0daf0c2316c1eec51a3cb7fe23e9
Faiz Mir <faizmir@Faizs-MacBook-Air.local>
2021-08-31 22:46:34 +0530

    first commit

--- myapplication/src/main/java/com/example/myapplication/Custom.java
+++ myapplication/src/main/java/com/example/myapplication/Custom.java
@@ -1,2 +1,8 @@
-package com.example.myapplication;public class Custom {
+package com.example.myapplication;
+
+public class Custom {
+
+    public static int add(int a, int b){
+        return a + b;
+    }
 }
